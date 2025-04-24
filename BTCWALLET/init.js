90809b74448fe99b72e8fa9fe80d07a2fd2c6259
ClaudioABittencourt <byttencourt@hotmail.com>
2025-04-24 20:19:11 -0300

    Update init.js

--- BTCWALLET/init.js
+++ BTCWALLET/init.js
@@ -9,3 +9,13 @@
 
 //marine collect desk capital index intact remember pact hidden labor similar amused 
 //cryptoboy
+
+// 1Carteira Gerada
+// Endereço:  tb1qq6q8gnlqyk4r6phh4z8nlpy7etlwn4l83x2xf6
+// Chave privada cV5zAR1Ajowy5MGPX28d17Sh3kZAbAdmRsp6CQPWecidgfuWQwcx
+// Seed:  hungry coral sudden aunt action diary clip trial page mean awkward topple
+
+// 2Carteira Gerada
+// Endereço:  tb1q5ggwhpjamkq3a0xqtk7d3r8tp59jt0nlvq42h9
+// Chave privada cTYSerxH8NetC71rUkqa2iUSQiUMirMymHFfjjPBsehWMDXiCwWU
+// Seed:  police consider quote couch salon jar cabin exercise genius inhale shadow walnut
